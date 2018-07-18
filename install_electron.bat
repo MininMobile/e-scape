d5bd76c06b42acb4009d2205ed88411206695990
MininMobile <notminin@gmail.com>
2018-07-18 13:49:01 +0200

    add assistance files

---
+++ install_electron.bat
@@ -0,0 +1,13 @@
+@echo off
+echo .
+echo INSTALLING ELECTRON
+echo ---
+echo This requires installation of node.js and npm
+echo.
+npm i electron -g
+echo.
+echo INSTALLATION COMPLETE
+echo ---
+echo Electron has probably been installed :tada:
+echo.
+pause>nul
