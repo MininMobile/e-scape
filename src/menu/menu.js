44f849c70e2a071eed5e7088406c20cb17d304f8
MininMobile <notminin@gmail.com>
2018-07-18 12:34:20 +0200

    add button functions

---
+++ src/menu/menu.js
@@ -0,0 +1,11 @@
+function play() {
+	window.location = "../console/console.html";
+}
+
+function settings() {
+	window.location = "../settings/settings.html";
+}
+
+function exit() {
+	window.close();
+}
