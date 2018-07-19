63813e579cc733fa8f24396f1848c820b16820f4
MininMobile <notminin@gmail.com>
2018-07-19 22:17:13 +0200

    merge menus

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -2,16 +2,19 @@ const storage = require("electron-json-storage");
 
 let _mainMenu = document.getElementById("mainmenu");
 
-function menu_show() { _mainMenu.classList.remove("base-hide"); }
-function menu_hide() { _mainMenu.classList.add("base-hide"); }
+function show_menu() { _mainMenu.classList.remove("base-hide"); }
+function hide_menu() { _mainMenu.classList.add("base-hide"); }
 
 function show_dialog(d) { d.classList.add("overlay-show"); }
 function hide_dialog(d) { d.classList.remove("overlay-show"); }
 
+function hidel_dialog(d) { d.classList.remove("overlay-show"); d.classList.add("base-hide"); }
+function showl_dialog(d) { d.classList.add("overlay-show"); d.classList.remove("base-hide"); }
+
 function format() {
 	storage.clear();
-	menu_show();
-	format_hide();
+	showl_dialog(d.settings);
+	hide_dialog(d.format)
 }
 
 function play() {
