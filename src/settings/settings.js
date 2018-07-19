81124336335a88efa6359526644a61c98eb597ee
MininMobile <notminin@gmail.com>
2018-07-19 18:20:35 +0200

    animate base menu

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,13 +1,18 @@
 const storage = require("electron-json-storage");
 const { dialog } = require('electron').remote;
 
-let _formatDialog = document.getElementById("dialog_format")
+let _mainMenu = document.getElementById("mainmenu");
+let _formatDialog = document.getElementById("dialog_format");
+
+function menu_show() { _mainMenu.classList.remove("base-hide"); }
+function menu_hide() { _mainMenu.classList.add("base-hide"); }
 
 function format_show() { _formatDialog.classList.add("overlay-show"); }
 function format_hide() { _formatDialog.classList.remove("overlay-show"); }
 
 function format() {
 	storage.clear();
+	menu_show();
 	format_hide();
 }
 
