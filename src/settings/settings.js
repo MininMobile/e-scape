60efca2f41a8d60302d7b4697ebb04c384c58a93
MininMobile <notminin@gmail.com>
2018-07-19 15:22:19 +0200

    add reset button

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,3 +1,29 @@
+const storage = require("electron-json-storage");
+const {dialog} = require('electron').remote;
+
+function reset() {
+	dialog.showMessageBox({
+		type: "error",
+		buttons: ["Ok", "Cancel"],
+		message: "PRESSING OK WILL DELETE ALL SAVED DATA"
+	}, (i) => {
+		if (i == 0) {
+			storage.clear();
+			utilALERT("Saves Wiped");
+		} else {
+			utilALERT("Action Cancelled");
+		}
+	});
+}
+
 function back() {
 	window.location = "../menu/menu.html";
+}
+
+function utilALERT(text) {
+	dialog.showMessageBox({
+		type: "info",
+		buttons: ["Dissmiss"],
+		message: text
+	}, (i) => { });
 }
